b5737c490e0f0156fd43604321685250722ac17e
slabys <simi.sla@seznam.cz>
2022-03-27 12:44:40 +0200

    Fixes

--- server/index.js
+++ server/index.js
@@ -10,9 +10,18 @@ const PORT = process.env.PORT || 5000
 
 import postsRoutes from './routes/posts.js'
 
-app.use('/posts', postsRoutes)
-app.use(cors());
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(cors());
+
+/**
+ * ROUTES
+ */
+
+app.use('/posts', postsRoutes)
+
+/**
+ * END ROUTES
+ */
 
 mongoose.connect(process.env.MONGODB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true,})
     .then(() => app.listen(PORT, () => {
