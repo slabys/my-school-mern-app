ba5a7ef2568e0b8db5f3744e0685e25f05748a94
slabys <simi.sla@seznam.cz>
2022-04-17 21:01:33 +0200

    Fix show data in modal

--- client/src/Application/Posts/PostModal.tsx
+++ client/src/Application/Posts/PostModal.tsx
@@ -1,128 +1,135 @@
 import React from 'react';
 import {
-  Avatar,
-  Box, BoxProps,
-  Dialog,
-  DialogContent,
-  DialogTitle,
-  Divider,
-  Grid,
-  IconButton,
-  ListItemAvatar,
-  ListItemText, Stack,
-  Typography,
+    Avatar,
+    Box, BoxProps,
+    Dialog,
+    DialogContent,
+    DialogTitle,
+    Divider,
+    Grid,
+    IconButton,
+    ListItemAvatar,
+    ListItemText, Stack,
+    Typography,
 } from '@mui/material';
-import { Close, Email, LocationCity, Phone } from '@mui/icons-material';
-import { useLocation } from 'wouter';
-import { PostData } from 'reducers';
+import {Close, Email, LocationCity, Phone} from '@mui/icons-material';
+import {useLocation} from 'wouter';
+import {PostData} from 'reducers';
 import image from 'images/image.jpg';
-import { formatDate } from 'utils/utils';
+import {formatDate} from 'utils/utils';
 
-export const PostModal: React.FunctionComponent<{ postItem: PostData, backlink: string }> = ({ postItem, backlink }) => {
-  const [, setLocation] = useLocation();
+export const PostModal: React.FunctionComponent<{ postItem: PostData, backlink: string }> = ({postItem, backlink}) => {
+    const [, setLocation] = useLocation();
 
-  return (
-    <Dialog maxWidth='lg' fullWidth open scroll='body' onClose={() => {
-      setLocation(backlink);
-    }}>
-      <DialogTitle>
-        <IconButton sx={{ position: 'absolute', top: '8px', right: '8px' }} onClick={() => {
-          setLocation(backlink);
+    return (
+        <Dialog maxWidth='lg' fullWidth open scroll='body' onClose={() => {
+            setLocation(backlink);
         }}>
-          <Close />
-        </IconButton>
-      </DialogTitle>
-      <DialogContent>
-        <ModalContent post={postItem} />
-      </DialogContent>
-    </Dialog>
-  );
+            <DialogTitle>
+                <IconButton sx={{position: 'absolute', top: '8px', right: '8px'}} onClick={() => {
+                    setLocation(backlink);
+                }}>
+                    <Close/>
+                </IconButton>
+            </DialogTitle>
+            <DialogContent>
+                <ModalContent post={postItem}/>
+            </DialogContent>
+        </Dialog>
+    );
 };
 
-const ModalContent: React.FunctionComponent<{ post: PostData }> = ({ post }) => {
-  return (
-    <Grid container spacing={2}>
-      <Grid item display='flex' flexDirection='column' xs={12} md={4}>
-        <Box width='100%'>
-          <img
-            src={image}
-            alt={post.title}
-            width='100%'
-            loading='lazy'
-          />
-        </Box>
-      </Grid>
-      <Grid item xs={12} md={8}>
-        <Typography variant='h4' paragraph={false}>{post.title}</Typography>
-        <Divider sx={{ marginBottom: 2 }} />
-        <Typography>
-          {post.description}
-        </Typography>
-      </Grid>
-      <Grid item xs={12} md={12}>
-        <Stack direction='row' sx={{ justifyContent: 'space-between'}}>
-          <Typography variant='h5' paragraph={false}>Info about product:</Typography>
-          <Box>
-            {
-              post.prize && post.prize === 'FREE' || post.prize === 'BUY'
-                ? <Typography>{post.prizeType}</Typography>
-                : <Typography>{post.prizeType}: {post.prize}</Typography>
-            }
-          </Box>
-        </Stack>
-        <Divider sx={{ marginBottom: 2 }} />
-        <Typography variant='body1'>Created
-          by <b>{post.creatorId.nickname}</b> on <i>{formatDate(new Date(post.createdAt))}</i></Typography>
-        <Box sx={{
-          display: 'flex',
-          flexWrap: 'wrap',
-        }}>
-          <Item>
-            <ListItemAvatar>
-              <Avatar>
-                <LocationCity />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary='City' secondary={post.creatorId.city} />
-          </Item>
-          <Item>
-            <ListItemAvatar>
-              <Avatar>
-                <Email />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary='Email' secondary={post.creatorId.email} />
-          </Item>
-          <Item>
-            <ListItemAvatar>
-              <Avatar>
-                <Phone />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary='Phone number' secondary={post.creatorId.phoneNumber} />
-          </Item>
-        </Box>
-      </Grid>
-    </Grid>
-  );
+const ModalContent: React.FunctionComponent<{ post: PostData }> = ({post}) => {
+    return (
+        <Grid container spacing={2}>
+            <Grid item display='flex' flexDirection='column' xs={12} md={4}>
+                <Box width='100%'>
+                    <img
+                        src={image}
+                        alt={post.title}
+                        width='100%'
+                        loading='lazy'
+                    />
+                </Box>
+            </Grid>
+            <Grid item xs={12} md={8}>
+                <Typography variant='h4' paragraph={false}>{post.title}</Typography>
+                <Divider sx={{marginBottom: 2}}/>
+                <Typography>
+                    {post.description}
+                </Typography>
+            </Grid>
+            <Grid item xs={12} md={12}>
+                <Stack direction='row' sx={{justifyContent: 'space-between'}}>
+                    <Typography variant='h5' paragraph={false}>Info about product:</Typography>
+                    <Box>
+                        {
+                            post.prize && post.prize === 'FREE' || post.prize === 'BUY'
+                                ? <Typography>{post.prizeType}</Typography>
+                                : <Typography>{post.prizeType}: {post.prize}</Typography>
+                        }
+                    </Box>
+                </Stack>
+                <Divider sx={{marginBottom: 2}}/>
+                <Typography variant='body1'>Created
+                    by <b>{post.creatorId.nickname}</b> on <i>{formatDate(new Date(post.createdAt))}</i></Typography>
+                <Box sx={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                }}>
+                    {post.creatorId.city ?
+                        <Item>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <LocationCity/>
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary='City' secondary={post.creatorId.city}/>
+                        </Item>
+                        : null}
+                    {post.creatorId.email ?
+                        <Item>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <Email/>
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary='Email' secondary={post.creatorId.email}/>
+
+                        </Item>
+                        : null}
+                    {post.creatorId.phoneNumber ?
+                        <Item>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <Phone/>
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary='Phone number' secondary={post.creatorId.phoneNumber}/>
+                        </Item>
+                        : null}
+                </Box>
+            </Grid>
+        </Grid>
+    );
 };
 
 const Item = (props: BoxProps) => {
-  const { sx, ...other } = props;
-  return (
-    <>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'row',
-          alignItems: 'center',
-          mixWidth: 32,
-          py: 1,
-          ...sx,
-        }}
-        {...other}
-      />
-      <Divider sx={{ mx: 3 }} orientation='vertical' flexItem />
-    </>
-  );
+    const {sx, ...other} = props;
+    return (
+        <>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    mixWidth: 32,
+                    py: 1,
+                    ...sx,
+                }}
+                {...other}
+            />
+            <Divider sx={{mx: 3}} orientation='vertical' flexItem/>
+        </>
+    );
 };
